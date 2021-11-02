2c2f6296dda9cb203d87531cfa02b811495e1a84
Neobee <88863739+iNeobee@users.noreply.github.com>
2021-11-02 20:07:09 +0700

    Update 1base.sh

--- 1base.sh
+++ 1base.sh
@@ -2,7 +2,8 @@
 #
 ## My arch install
 #
-# Swapfile
+
+echo "=====:=====:===== Make a Swapfile =====:=====:====="
 dd if=/dev/zero of=/swap/swapfile bs=1G count=2 status=progress
 chmod 600 /swap/swapfile
 mkswap /swap/swapfile
@@ -11,15 +12,15 @@ echo "" >> /etc/fstab
 echo "# /swap/swapfile" >> /etc/fstab
 echo "/swap/swapfile    none    swap    defaults    0 0" >> /etc/fstab
 
-# Swappiness
+echo "=====:=====:===== Configure Swappiness =====:=====:====="
 touch /etc/sysctl.d/99-swappiness.conf
 echo "vm.swappiness=10" >> /etc/sysctl.d/99-swappiness.conf
 
-# Timezone
+echo "=====:=====:===== Configure Timezone =====:=====:====="
 ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 hwclock --systohc
 
-# Locale
+echo "=====:=====:===== Configure Locale =====:=====:====="
 sed -i '177s/.//' /etc/locale.gen # en_US: change line 177 first char with nothing :)
 sed -i '289s/.//' /etc/locale.gen # id_ID
 sed -i '302s/.//' /etc/locale.gen # ja_JP
@@ -27,26 +28,27 @@ sed -i '313s/.//' /etc/locale.gen # ko_KR
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
-# Hostname & hosts : Change the host name to what u want
+echo "=====:=====:===== Configuring Hostname & Hosts =====:=====:====="
 echo "urHostName" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 urHostName.localdomain urHostName" >> /etc/hosts
 
-# Base packages
+echo "=====:=====:===== Install Base Package I Need =====:=====:====="
 pacman -Syu --needed grub efibootmgr networkmanager ufw alsa-utils pulseaudio{,-alsa} bash-completion dbus-broker xdg-{utils,user-dirs} bat htop lsd neofetch
 
-# Installing grub
+echo "=====:=====:===== Installing grub =====:=====:====="
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+echo "=====:=====:===== Generate Grub Config =====:=====:====="
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# enabling some services
+echo "=====:=====:===== Enabling Some Service =====:=====:====="
 systemctl enable NetworkManager
 systemctl enable ufw
 systemctl disable dbus.service
 systemctl enable dbus-broker.service
 
-# xdg user dirs
+echo "=====:=====:===== Configuring XDG User Dirs =====:=====:====="
 sed -i 's/Desktop/desktop/' /etc/xdg/user-dirs.defaults
 sed -i 's/Downloads/my\/downloads/' /etc/xdg/user-dirs.defaults
 sed -i 's/Templates/my\/templates/' /etc/xdg/user-dirs.defaults
@@ -56,8 +58,21 @@ sed -i 's/Music/my\/music/' /etc/xdg/user-dirs.defaults
 sed -i 's/Pictures/my\/pictures/' /etc/xdg/user-dirs.defaults
 sed -i 's/Videos/my\/videos/' /etc/xdg/user-dirs.defaults
 
+echo "=====:=====:===== Change Your Root Password =====:=====:====="
+passwd
+# Add a user
+echo "=====:=====:===== Adding a user =====:=====:====="
+useradd -mG wheel user
+echo "=====:=====:===== Change Your User Password =====:=====:====="
+passwd user
+EDITOR=vim visudo
+
+echo "=====:=====:===== Copy Scripts To User Home Dirs =====:=====:====="
+cp -r /tmp/arch /home/user/
+chown -R user:user /home/user/arch
+echo "=====:=====:===== Done Copying The Scripts =====:=====:====="
+ls -l /home/user/
+
 echo "=====:=====:===== Things Left =====:=====:====="
-echo "Change root passwd: passwd"
-echo "Add a user        : useradd -mG wheel ur_username"
-echo "Edit visudo       : EDITOR=vim visudo"
-echo "                    exit umount & reboot. DONE :)"
+echo "         exit umount & reboot. DONE :)         "
+echo "=====:=====:===== ----------- =====:=====:====="
