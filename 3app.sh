a774f23aaae96714391dcfee1f5132c522f7f5c0
Neobee <88863739+iNeobee@users.noreply.github.com>
2021-11-04 09:05:42 +0700

    Update 3app.sh

--- 3app.sh
+++ 3app.sh
@@ -16,29 +16,6 @@ sudo pacman -S --needed polkit lxsession xorg-{server,xinit,xrandr,xbacklight,xp
 
 sudo fc-cache -fv
 
-echo "=====:=====:=====       Installing unimatrix         =====:=====:====="
-sudo curl -L https://raw.githubusercontent.com/will8211/unimatrix/master/unimatrix.py -o /usr/local/bin/unimatrix
-sudo chmod a+rx /usr/local/bin/unimatrix
-
-echo "=====:=====:=====       Installing Vim Plugins       =====:=====:====="
-git clone https://github.com/lilydjwg/colorizer .config/vim/pack/plugins/start/colorizer/
-git clone https://github.com/vim-airline/vim-airline .config/vim/pack/plugins/start/vim-airline/
-git clone https://github.com/vim-airline/vim-airline-themes .config/vim/pack/plugins/start/vim-airline-themes/
-git clone https://github.com/vim-syntastic/syntastic .config/vim/pack/plugins/start/syntastic/
-git clone https://github.com/jiangmiao/auto-pairs .config/vim/pack/plugins/start/auto-pairs/
-git clone https://github.com/alvan/vim-closetag .config/vim/pack/plugins/start/vim-closetag/
-git clone https://github.com/tpope/vim-commentary .config/vim/pack/plugins/start/vim-commentary/
-git clone https://github.com/tpope/vim-surround .config/vim/pack/plugins/start/vim-surround/
-git clone https://github.com/honza/vim-snippets .config/vim/pack/plugins/start/vim-snippets/
-git clone https://github.com/SirVer/ultisnips .config/vim/pack/plugins/start/ultisnips/
-git clone https://github.com/lervag/vimtex .config/vim/pack/plugins/start/vimtex/
-git clone https://github.com/mg979/vim-visual-multi .config/vim/pack/plugins/start/vim-visual-multi/
-
-echo "=====:=====:=====  Installing Vim Nord Color Scheme  =====:=====:====="
-git clone https://github.com/arcticicestudio/nord-vim my/downloads/clone/nord-vim/
-cp -r my/downloads/clone/nord-vim/colors .config/vim/
-cp my/downloads/clone/nord-vim/autoload/airline/themes/* .config/vim/pack/plugins/start/vim-airline-themes/autoload/airline/themes/
-
 echo "=====:=====:=====           Rebuilding Vim           =====:=====:====="
 cd my/downloads/clone/vim-git/
 sed -i '46s/disable/enable/' PKGBUILD
@@ -46,7 +23,7 @@ sudo pacman -Rns vim
 makepkg -si
 
 echo "=====:=====:===== Installing My Core Packages Part 2 =====:=====:====="
-sudo pacman -S --needed xmonad{,-contrib} xmobar python-pillow kitty{,-terminfo} && sudo aura -Ax google-chrome && sudo aura -Ax spacefm && sudo pacman -S --needed nitrogen picom dmenu rofi scrot yt-dlp && sudo aura -Ax i3lock-color && sudo pacman -S --needed lxappearance qt5ct arc-gtk-theme kvantum-qt5 papirus-icon-theme imagemagick sxiv mpv celluloid ffmpeg tar unzip zip && sudo aura -Ax rar && sudo pacman -S --needed file-roller udisks2 udevil ntfs-3g gvfs{,-mtp} libmtp mtpfs && sudo aura -Ax jmtpfs && sudo pacman -S --needed libreoffice-fresh gimp inkscape obs-studio && sudo aura -Ax olive && sudo pacman -S --needed zathura{,-cb,-djvu,-pdf-mupdf,-ps}
+sudo pacman -S --needed xmonad{,-contrib} xmobar python-pillow kitty{,-terminfo} && sudo aura -Ax google-chrome && sudo aura -Ax spacefm && sudo pacman -S --needed nitrogen picom rofi scrot yt-dlp && sudo aura -Ax i3lock-color && sudo pacman -S --needed lxappearance qt5ct arc-gtk-theme kvantum-qt5 papirus-icon-theme imagemagick sxiv mpv celluloid ffmpeg tar unzip zip && sudo aura -Ax rar && sudo pacman -S --needed file-roller udisks2 udevil ntfs-3g gvfs{,-mtp} libmtp mtpfs && sudo aura -Ax jmtpfs && sudo pacman -S --needed libreoffice-fresh gimp inkscape obs-studio && sudo aura -Ax olive && sudo pacman -S --needed zathura{,-cb,-djvu,-pdf-mupdf,-ps}
 
 echo "=====:=====:=====           Installing Tex           =====:=====:====="
 sudo pacman -S  texlive{-most,-lang,-bibtexextra}
